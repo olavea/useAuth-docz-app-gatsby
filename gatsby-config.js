8a5e32c98c9e77c467366325db96f96f0d881df4
Ola Vea <ola@olavea.com>
2020-01-10 14:36:08 +0100

    rebuild with docz-app-gatsby #6

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  siteMetadata: {
+    title: `useAuth`,
+    description: `Add authentication to your React app.`,
+    twitter: "@swizec",
+    image: "https://twitter.com/Swizec/photo"
+  },
+
+  plugins: [
+    "gatsby-theme-docz",
+    `gatsby-transformer-sharp`
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ]
+};
