5d30e1db141ebce3f8811c05006440071942e259
Ola Vea <ola@olavea.com>
2020-01-12 16:48:06 +0100

    favicon added

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,18 @@ module.exports = {
 
   plugins: [
     "gatsby-theme-docz",
-    `gatsby-transformer-sharp`
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `useAuth!`,
+        short_name: `useAuth!`,
+        start_url: `/`,
+
+        icon: `src/images/Swizec2.png`
+      }
+    }
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
