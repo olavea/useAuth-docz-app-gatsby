350083b86f2853f4ed8842252ad3957d5fdb5af1
Ola Vea <ola@olavea.com>
2020-01-14 11:18:20 +0100

    added exampleLogin.js #6

--- src/pages/exampleLogin.js
+++ src/pages/exampleLogin.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Button } from "../components/Button";
 import { useAuth } from "react-use-auth";
 
-const NotFoundPage = () => {
+const Login = () => {
   const { isAuthenticated, login, logout } = useAuth();
   if (isAuthenticated()) {
     return <Button onClick={logout}>Logout</Button>;
@@ -10,4 +10,5 @@ const NotFoundPage = () => {
     return <Button onClick={login}>Login</Button>;
   }
 };
-export default NotFoundPage;
+
+export default Login;
